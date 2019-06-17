1ad745ae6efabbcf6a6b40a2176fd6f8fd6b056c
Jason Bourne <m.bourne@wi.rr.com>
2019-06-17 15:56:32 -0500

    added mysql access

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,2 +1,20 @@
 var mysql = require("mysql");
 var inquirer = require("inquirer");
+
+var con = mysql.createConnection({
+    user: 'root',
+    password: 'password',
+    database: 'bamazon_db',
+    host: 3306
+  });
+
+con.connect(function(err) {
+    if (err) throw err;
+    console.log("Connected");
+    con.query("SELECT * FROM products", function (err, result) {
+        if (err) throw err;
+        console.log(result);
+    });
+});
+
+
